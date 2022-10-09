0a54c29ad536d42d5d376d370d07332cad274379
Severin Reifler <severin@reifler.net>
2022-10-09 13:16:28 +0200

    parallax stuff

--- components/ParallaxParent.tsx
+++ components/ParallaxParent.tsx
@@ -4,10 +4,10 @@ import ParallaxWrapper from './ParallaxWrapper';
 const ParallaxParent = () => {
   const tears = [
     { top: 'top-[100vh]', left: 'left-[70vw]', speed: -30, type: 'tear' },
-    { top: 'top-[15vh]', left: 'left-[2vw]', speed: -50, type: 'tear' },
-    { top: 'top-[220vh]', left: 'left-[0vw]', speed: -55, type: 'tear' },
-    { top: 'top-[200vh]', left: 'left-[80vw]', speed: -40, type: 'tear' },
-    { top: 'top-[300vh]', left: 'left-[80vw]', speed: -80, type: 'tear' },
+    { top: 'top-[400vh]', left: 'left-[12vw]', speed: -130, type: 'tear' },
+    { top: 'top-[200vh]', left: 'left-[0vw]', speed: -55, type: 'tear' },
+    // { top: 'top-[200vh]', left: 'left-[80vw]', speed: -40, type: 'tear' },
+    { top: 'top-[320vh]', left: 'left-[80vw]', speed: -80, type: 'tear' },
     { top: 'top-[100vh]', left: 'left-[50vw]', speed: -150, type: 'rootV' },
     { top: 'top-[350vh]', left: 'left-[40vw]', speed: -150, type: 'rootH' },
   ];
